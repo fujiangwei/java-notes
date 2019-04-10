bd78b73629f1e37dffffd045f231c4b989094ff3
kinsonfu <2444179219@qq.com>
2019-04-10 13:27:35 +0800

    类加载器

---
+++ src/main/java/com/notes/java/classloader/ClassLoaderTest.java
@@ -0,0 +1,22 @@
+package com.notes.java.classloader;
+
+/**
+ * descripiton: 类加载器
+ *
+ * @author: kinson(2219945910@qq.com)
+ * @date: 2019/4/9
+ * @time: 14:04
+ * @modifier:
+ * @since:
+ */
+public class ClassLoaderTest {
+
+    public static void main(String[] args) {
+        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+        System.out.println("systemClassLoader -> " + systemClassLoader);
+        ClassLoader systemParent = systemClassLoader.getParent();
+        System.out.println("systemParentClassLoader -> " + systemParent);
+        ClassLoader systemParentParent = systemParent.getParent();
+        System.out.println("systemParentParentClassLoader -> " + systemParentParent);
+    }
+}
