1a0a7de3ff97b739e9474ed432f8c741f8a2c3b0
fujiangwei <2444179219@qq.com>
2020-04-16 18:58:11 +0800

    Junit4+MultiThreadedTestRunner 并发测试

--- src/test/java/JavaNotesApplicationTest.java
+++ src/test/java/JavaNotesApplicationTest.java
@@ -3,6 +3,8 @@ import com.notes.service.UserService;
 import com.notes.utils.RedisDistributeLock;
 import com.notes.utils.SpringContextUtil;
 import com.notes.utils.ZKDistributeLock;
+import net.sourceforge.groboutils.junit.v1.MultiThreadedTestRunner;
+import net.sourceforge.groboutils.junit.v1.TestRunnable;
 import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 文件描述 测试类
@@ -72,13 +75,15 @@ public class JavaNotesApplicationTest {
         int count = 10;
         CountDownLatch countDownLatch = new CountDownLatch(count);
         try {
+            ZKDistributeLock zkDistributeLock = new ZKDistributeLock("zk_distribute_lock");
             for (int i = 0; i < count; i ++) {
                 new Thread(() -> {
-                    ZKDistributeLock zkDistributeLock = new ZKDistributeLock("zk_distribute_lock");
                     boolean tryLock = zkDistributeLock.tryLock("zk_distribute_lock");
                     try {
-                        Thread.sleep(RandomUtils.nextInt(2000, 5000));
-                        boolean releaseLock = zkDistributeLock.releaseLock("zk_distribute_lock");
+                        if (tryLock) {
+                            Thread.sleep(RandomUtils.nextInt(2000, 5000));
+//                            boolean releaseLock = zkDistributeLock.releaseLock("zk_distribute_lock");
+                        }
                         countDownLatch.countDown();
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -87,6 +92,49 @@ public class JavaNotesApplicationTest {
             }
             countDownLatch.await();
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 用来并发计数 调了多少次方法
+    private static int num;
+    private static AtomicInteger count = new AtomicInteger();
+
+    @Test
+    public void paralleTest() {
+        TestRunnable runnable = new TestRunnable() {
+            @Override
+            public void runTest() throws Throwable {
+                long startTime = System.currentTimeMillis();
+
+                count.incrementAndGet();
+                num = count.get();
+                System.out.println(Thread.currentThread() + " runTest:" + num + " time is " + (System.currentTimeMillis() - startTime));
+            }
+        };
+
+        // time 并发时间 单位秒 runnerCount 并发数 100
+        int time = 10;
+        int runnerCount = 100;
+        TestRunnable[] testRunnables = new TestRunnable[runnerCount];
+        for (int i = 0; i < runnerCount; i++) {
+            testRunnables[i] = runnable;
+        }
+
+        MultiThreadedTestRunner multiThreadedTestRunner = new MultiThreadedTestRunner(testRunnables);
+
+        try {
+            long startTime = System.currentTimeMillis();
+            while (true) {
+                long endTime = System.currentTimeMillis();
+                multiThreadedTestRunner.runTestRunnables();
+                // 并发运行 放在while死循环里 当时间到了，跳出循环
+                if ((endTime - startTime) > time * 1000) {
+                    System.out.println("over:" + num);
+                    break;
+                }
+            }
+        } catch (Throwable e) {
             e.printStackTrace();
         }
     }
