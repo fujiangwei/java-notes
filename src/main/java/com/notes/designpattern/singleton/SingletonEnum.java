433f97c4d2e04a309a3dfdab6d4fb34c2bdc9a2d
fujiangwei <2444179219@qq.com>
2020-05-29 15:53:53 +0800

    单例模式实现：枚举或者IOC类缓存容器控制

--- src/main/java/com/notes/designpattern/singleton/SingletonEnum.java
+++ src/main/java/com/notes/designpattern/singleton/SingletonEnum.java
@@ -1,20 +1,7 @@
 package com.notes.designpattern.singleton;
 
 /**
- * 文件描述
- *
- * @ProductName: Hundsun HEP
- * @ProjectName: java-notes
- * @Package: com.notes.designpattern.singleton
- * @Description: note
- * @Author: fujw27320
- * @CreateDate: 2020/5/29 15:48
- * @UpdateUser: fujw27320
- * @UpdateDate: 2020/5/29 15:48
- * @UpdateRemark: The modified content
- * @Version: 1.0
- * <p>
- * Copyright © 2020 Hundsun Technologies Inc. All Rights Reserved
+ * 文件描述 枚举单例
  **/
 public enum SingletonEnum {
 
